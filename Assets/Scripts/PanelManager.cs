8c65055f1d8ed9ddb5e1e7dafec38249609328ec
DatBoi02 <97614174+AndreBoi02@users.noreply.github.com>
2024-02-28 17:18:58 -0600

    UI.2 Commit
    UI is now finished

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class PanelManager : MonoBehaviour
 {
-    
+    public GameObject[] panels;
+    GameObject currentPanel;
+
+    public void EnablePanel(int index)
+    {
+        if (index != 0) panels[0].SetActive(false);
+        panels[index].SetActive(true);
+        currentPanel = panels[index];
+    }
+
+    public void Return()
+    {
+        if (currentPanel == null) return;
+        currentPanel.SetActive(false);
+        panels[0].SetActive(true);
+    }
 }
